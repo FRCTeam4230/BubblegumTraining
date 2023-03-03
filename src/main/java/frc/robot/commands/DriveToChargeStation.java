0afdc46c1d6f8bb7c605e3bf8d5c4e093201c181
ZiyahX <93620681+ZiyahX@users.noreply.github.com>
2023-03-02 21:26:35 -0600

    Added drive to charge station that uses pitch

--- src/main/java/frc/robot/commands/DriveToChargeStation.java
+++ src/main/java/frc/robot/commands/DriveToChargeStation.java
@@ -4,33 +4,50 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
 public class DriveToChargeStation extends CommandBase {
-  DriveTrainSubsystem driveTrain;
+  private DriveTrainSubsystem driveTrain;
+  private PIDController rotationPIDController;
   /** Creates a new DriveToChargeStation. */
   public DriveToChargeStation(DriveTrainSubsystem driveTrain) {
     this.driveTrain = driveTrain;
+    rotationPIDController = new PIDController(
+      Constants.DriveTrain.TURN_KP,
+      Constants.DriveTrain.TURN_KI,
+      Constants.DriveTrain.TURN_KD);
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveTrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    driveTrain.zeroHeading();
+    rotationPIDController.setSetpoint(0);
+    rotationPIDController.setTolerance(1.5);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    double output = rotationPIDController.calculate(driveTrain.getRawHeading());
+    driveTrain.arcadeDrive(-0.6, MathUtil.clamp(output, -0.2, 0.2));
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    driveTrain.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return driveTrain.getPitch() <= -Constants.AutoConstants.CHARGE_STATION_ONTO_PITCH;
   }
 }
